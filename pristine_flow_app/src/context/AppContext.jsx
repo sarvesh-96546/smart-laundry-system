aa3af630f002ec0d6281e5b79d68ed272c977bdf
Lucky9352 <luckyagarwal9079@gmail.com>
2026-04-02 13:53:42 +0530

    IC

--- pristine_flow_app/src/context/AppContext.jsx
+++ pristine_flow_app/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 import toast, { Toaster } from 'react-hot-toast';
 import { authClient, useSession } from '../lib/auth-client';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_URL || "";
 
 import { AppContext } from './useApp';
 
